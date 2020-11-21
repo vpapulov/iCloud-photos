9d2e2130b82242251c0034c8f541a568dadfa719
Vladlen Papulov <vladlen.papulov@gmail.com>
2020-11-21 21:07:55 +0300

    working example, but problems with verification code

---
+++ main.py
@@ -0,0 +1,99 @@
+import contextlib
+import http.client
+import logging
+import sys
+
+import click
+import requests
+import warnings
+from pprint import pprint
+from pyicloud import PyiCloudService
+from urllib3.exceptions import InsecureRequestWarning
+
+from config import ICLOUD_LOGIN
+
+# Handle certificate warnings by ignoring them
+old_merge_environment_settings = requests.Session.merge_environment_settings
+
+
+@contextlib.contextmanager
+def no_ssl_verification():
+    opened_adapters = set()
+
+    def merge_environment_settings(self, url, proxies, stream, verify, cert):
+        # Verification happens only once per connection so we need to close
+        # all the opened adapters once we're done. Otherwise, the effects of
+        # verify=False persist beyond the end of this context manager.
+        opened_adapters.add(self.get_adapter(url))
+
+        settings = old_merge_environment_settings(
+            self, url, proxies, stream, verify, cert
+        )
+        settings["verify"] = False
+
+        return settings
+
+    requests.Session.merge_environment_settings = merge_environment_settings
+
+    try:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", InsecureRequestWarning)
+            yield
+    finally:
+        requests.Session.merge_environment_settings = old_merge_environment_settings
+
+        for adapter in opened_adapters:
+            try:
+                adapter.close()
+            except:
+                pass
+
+
+# Monkeypatch the http client for full debugging output
+httpclient_logger = logging.getLogger("http.client")
+
+
+def httpclient_logging_patch(level=logging.DEBUG):
+    """Enable HTTPConnection debug logging to the logging framework"""
+
+    def httpclient_log(*args):
+        httpclient_logger.log(level, " ".join(args))
+
+    # mask the print() built-in in the http.client module to use
+    # logging instead
+    http.client.print = httpclient_log
+    # enable debugging
+    http.client.HTTPConnection.debuglevel = 1
+
+
+# Enable general debug logging
+logging.basicConfig(level=logging.DEBUG)
+
+httpclient_logging_patch()
+password = input('Password:')
+
+api = PyiCloudService(ICLOUD_LOGIN, password)
+if api.requires_2sa:
+    print("Two-factor authentication required. Your trusted devices are:")
+
+    devices = api.trusted_devices
+    for i, device in enumerate(devices):
+        print(
+            "  %s: %s"
+            % (i, device.get("deviceName", "SMS to %s") % device.get("phoneNumber"))
+        )
+
+    device = click.prompt("Which device would you like to use?", default=0)
+    device = devices[device]
+    if not api.send_verification_code(device):
+        print("Failed to send verification code")
+        sys.exit(1)
+
+    code = click.prompt("Please enter validation code")
+    if not api.validate_verification_code(device, code):
+        print("Failed to verify verification code")
+        sys.exit(1)
+
+# This request will not fail, even if using intercepting proxies.
+with no_ssl_verification():
+    pprint(api.account)
